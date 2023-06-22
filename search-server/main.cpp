a9941450ea09d20903fd20d2f9e0d755f7dafa05
CyberCat <Vgnetnet156@gmail.com>
2023-06-22 12:18:25 +0300

    Creating GetDocumentCount  function, modified main

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -90,6 +90,16 @@ public:
         return matched_documents;
     }
 
+    int GetDocumentCount() const
+    {
+        return static_cast<int>(documents_.size());
+    }
+
+    tuple<vector<string>, DocumentStatus> MatchDocument(const string& raw_query, int document_id) const
+    {
+        
+    }
+
 private:
     struct DocumentData {
         int rating;
@@ -97,8 +107,8 @@ private:
     };
 
     set<string> stop_words_;
-    map<string, map<int, double>> word_to_document_freqs_;
-    map<int, DocumentData> documents_;
+    map<string, map<int, double>> word_to_document_freqs_; // слова, id документов, tf
+    map<int, DocumentData> documents_; // id, рейтинг, статус
 
     bool IsStopWord(const string& word) const {
         return stop_words_.count(word) > 0;
@@ -198,14 +208,16 @@ private:
     }
 };
 
-void PrintDocument(const Document& document) {
+void PrintMatchDocumentResult(int document_id, const vector<string>& words, DocumentStatus status) {
     cout << "{ "s
-         << "document_id = "s << document.id << ", "s
-         << "relevance = "s << document.relevance << ", "s
-         << "rating = "s << document.rating
-         << " }"s << endl;
+         << "document_id = "s << document_id << ", "s
+         << "status = "s << static_cast<int>(status) << ", "s
+         << "words ="s;
+    for (const string& word : words) {
+        cout << ' ' << word;
+    }
+    cout << "}"s << endl;
 }
-
 int main() {
     SearchServer search_server;
     search_server.SetStopWords("и в на"s);
@@ -213,13 +225,9 @@ int main() {
     search_server.AddDocument(1, "пушистый кот пушистый хвост"s,       DocumentStatus::ACTUAL, {7, 2, 7});
     search_server.AddDocument(2, "ухоженный пёс выразительные глаза"s, DocumentStatus::ACTUAL, {5, -12, 2, 1});
     search_server.AddDocument(3, "ухоженный скворец евгений"s,         DocumentStatus::BANNED, {9});
-    cout << "ACTUAL:"s << endl;
-    for (const Document& document : search_server.FindTopDocuments("пушистый ухоженный кот"s, DocumentStatus::ACTUAL)) {
-        PrintDocument(document);
+    const int document_count = search_server.GetDocumentCount();
+    for (int document_id = 0; document_id < document_count; ++document_id) {
+        const auto [words, status] = search_server.MatchDocument("пушистый кот"s, document_id);
+        PrintMatchDocumentResult(document_id, words, status);
     }
-    cout << "BANNED:"s << endl;
-    for (const Document& document : search_server.FindTopDocuments("пушистый ухоженный кот"s, DocumentStatus::BANNED)) {
-        PrintDocument(document);
-    }
-    return 0;
-}
+} 
