171256581941eeff2f5b80373124f775c619144e
CyberCat <Vgnetnet156@gmail.com>
2023-07-16 14:14:55 +0300

    Added MatchDocument function

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -122,7 +122,30 @@ public:
         return static_cast<int>(documents_.size());
     }
 
-    
+    tuple<vector<string>, DocumentStatus> MatchDocument(const string& raw_query, int document_id) const 
+    {
+        const Query query = ParseQuery(raw_query);
+        vector<string> matched_words;
+        for (const string& word : query.plus_words) {
+            if (word_to_document_freqs_.count(word) == 0) {
+                continue;
+            }
+            if (word_to_document_freqs_.at(word).count(document_id)) {
+                matched_words.push_back(word);
+            }
+        }
+        for (const string& word : query.minus_words) {
+            if (word_to_document_freqs_.count(word) == 0) {
+                continue;
+            }
+            if (word_to_document_freqs_.at(word).count(document_id)) {
+                matched_words.clear();
+                break;
+            }
+        }
+        return {matched_words, documents_.at(document_id).status};
+    }
+
 private:
     struct DocumentData {
         int rating;
