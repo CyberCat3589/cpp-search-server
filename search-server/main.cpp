dde6783f25ef566ac88ef6078cb866541b126932
CyberCat <Vgnetnet156@gmail.com>
2023-08-14 23:42:27 +0300

    Modified main function

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -114,17 +114,17 @@ public:
 
     auto begin()
     {
-        return pages.begin();
+        return pages_.begin();
     }
 
     auto end()
     {
-        return pages.end();
+        return pages_.end();
     }
 
     size_t size()
     {
-        return pages.size();
+        return pages_.size();
     }
 
 private:
@@ -441,5 +441,19 @@ auto Paginate(Container& container, size_t page_size)
 
 int main() 
 {
-    
+    SearchServer search_server("and with"s);
+    search_server.AddDocument(1, "funny pet and nasty rat"s, DocumentStatus::ACTUAL, {7, 2, 7});
+    search_server.AddDocument(2, "funny pet with curly hair"s, DocumentStatus::ACTUAL, {1, 2, 3});
+    search_server.AddDocument(3, "big cat nasty hair"s, DocumentStatus::ACTUAL, {1, 2, 8});
+    search_server.AddDocument(4, "big dog cat Vladislav"s, DocumentStatus::ACTUAL, {1, 3, 2});
+    search_server.AddDocument(5, "big dog hamster Borya"s, DocumentStatus::ACTUAL, {1, 1, 1});
+    const auto search_results = search_server.FindTopDocuments("curly dog"s);
+    int page_size = 2;
+    const auto pages = Paginate(search_results, page_size);
+    // Выводим найденные документы по страницам
+    for (auto page = pages.begin(); page != pages.end(); ++page) 
+    {
+        cout << *page << endl;
+        cout << "Page break"s << endl;
+    }
 } 
