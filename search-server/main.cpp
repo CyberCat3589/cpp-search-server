ec6fab761d0e0bb6f144083a53d34ecbcace1ce7
CyberCat <Vgnetnet156@gmail.com>
2023-08-24 21:22:36 +0300

    Updated the Request Queue class

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -171,52 +171,6 @@ ostream& operator<<(ostream& out, vector<IteratorRange<Iterator>> iter_vec)
     return out;
 }
 
-class RequestQueue 
-{
-
-public:
-
-    explicit RequestQueue(const SearchServer& search_server) : server_(search_server)
-    {
-        current_time_ = 0;
-    }
-
-    // сделаем "обёртки" для всех методов поиска, чтобы сохранять результаты для нашей статистики
-    template <typename DocumentPredicate>
-    vector<Document> AddFindRequest(const string& raw_query, DocumentPredicate document_predicate) 
-    {
-        // напишите реализацию
-    }
-
-    vector<Document> AddFindRequest(const string& raw_query, DocumentStatus status) 
-    {
-        // напишите реализацию
-    }
-
-    vector<Document> AddFindRequest(const string& raw_query) 
-    {
-        // напишите реализацию
-    }
-
-    int GetNoResultRequests() const 
-    {
-        return no_result_count_;
-    }
-
-private:
-
-    struct QueryResult 
-    {
-        // определите, что должно быть в структуре
-    };
-
-    SearchServer server_;
-    deque<QueryResult> requests_;
-    const static int min_in_day_ = 1440;
-    unsigned long current_time_;
-    int no_result_count_;
-};
-
 class SearchServer 
 {
 public:
@@ -442,6 +396,79 @@ private:
                 {document_id, relevance, documents_.at(document_id).rating});
         }
         return matched_documents;
+    }
+};
+
+class RequestQueue 
+{
+
+public:
+
+    explicit RequestQueue(const SearchServer& search_server) 
+    : server_(search_server), 
+    no_results_requests_(0), 
+    current_time_(0) {}
+
+    // сделаем "обёртки" для всех методов поиска, чтобы сохранять результаты для нашей статистики
+    template <typename DocumentPredicate>
+    vector<Document> AddFindRequest(const string& raw_query, DocumentPredicate document_predicate) 
+    {
+        const auto result = server_.FindTopDocuments(raw_query, document_predicate);
+        AddRequest(result.size());
+        return result;
+    }
+
+    vector<Document> AddFindRequest(const string& raw_query, DocumentStatus status) 
+    {
+        const auto result = server_.FindTopDocuments(raw_query, status);
+        AddRequest(result.size());
+        return result;
+    }
+
+    vector<Document> AddFindRequest(const string& raw_query) 
+    {
+        const auto result = server_.FindTopDocuments(raw_query);
+        AddRequest(result.size());
+        return result;
+    }
+
+    int GetNoResultRequests() const 
+    {
+        return no_results_requests_;
+    }
+
+private:
+
+    struct QueryResult 
+    {
+        uint64_t timestamp;
+        int results;
+    };
+
+    SearchServer server_;
+    deque<QueryResult> requests_;
+    const static int min_in_day_ = 1440;
+    unsigned long current_time_;
+    int no_results_requests_;
+
+    void AddRequest(int results_num)
+    {
+        // новый запрос - новая секунда
+        ++current_time_;
+        // удаляем все результаты поиска, которые устарели
+        while (!requests_.empty() && min_in_day_ <= current_time_ - requests_.front().timestamp) 
+        {
+            if (0 == requests_.front().results) 
+            {
+                --no_results_requests_;
+            }
+            requests_.pop_front();
+        }
+        // сохраняем новый результат поиска
+        requests_.push_back({current_time_, results_num});
+        if (0 == results_num) {
+            ++no_results_requests_;
+        }
     }
 };
 
